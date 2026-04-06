acebb6bf6be9ecd6235a9bf8a5121c3a064f5d49
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-04-06 11:50:39 +0200

    add environment variables for login details

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,13 @@ import time
 import psycopg2
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+#Dane logowania ze zmiennych środowiskowych
+DB_HOST = os.environ.get("DB_HOST", "db")
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASS = os.environ.get("DB_PASSWORD", "postgres")
+DB_NAME = os.environ.get("DB_NAME", "tododb")
+
