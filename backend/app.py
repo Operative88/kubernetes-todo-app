88cd17f64bc72b85f3f774eb0d3d8ecc6123d222
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-04-06 12:11:49 +0200

    add get db connection method

--- backend/app.py
+++ backend/app.py
@@ -13,3 +13,16 @@ DB_USER = os.environ.get("DB_USER", "postgres")
 DB_PASS = os.environ.get("DB_PASSWORD", "postgres")
 DB_NAME = os.environ.get("DB_NAME", "tododb")
 
+def get_db_connection():
+    #Mechanizm ponawiania połączenia
+    retries = 5
+    while retries > 0:
+        try:
+            conn = psycopg2.connect(
+                host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS    
+            )
+            return conn
+        except psycopg2.OperationalError:
+            retries -= 1
+            time.sleep(2)
+    raise Exception("Nie można połączyć się z bazą danych")
